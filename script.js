451d65df358c5ef48e54c37828d73d9177e580d1
Michael Chan <michaelalexanderchan@gmail.com>
2022-11-24 17:21:48 -0500

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+//Returns a value between 1 - 3 that represents the computer hand choice
 function getComputerChoice() {
     return Math.floor(Math.random() * 3) + 1
 }
 
+//Returns a string for the hand choice based on the given number argument
 function getHandChoice(number) {
     let hand
     if (number === 1) {
@@ -19,6 +21,7 @@ function getHandChoice(number) {
     return hand
 }
 
+//Compares the player's choice to the computer's. If the player won, return true. Otherwise, return false
 function playerWin(playerChoice, computerChoice) {
     let playerWin
     if (playerChoice === "Rock") {
@@ -51,22 +54,53 @@ function playerWin(playerChoice, computerChoice) {
     return playerWin
 }
 
+//Plays a round of rock, paper scissors. Calls the PlayerWin function and logs the winner in the console. Returns 1 for player win, 0 for computer win
 function playRound(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
         console.log(`It's a tie!`)
+        return -1
     }
     else if (playerWin(playerChoice, computerChoice)) {
         console.log(`You win! ${playerChoice} beats ${computerChoice}`)
+        return 1
     }
     else {
-        console.log(`You lose! ${computerChoice} beats ${playerChoice}`)
+        console.log(`You lose! ${computerChoice} beats ${playerChoice}`) 
+        return 0
     }
 }
 
-const playerChoice = getHandChoice(1)
-const computerChoice = getHandChoice(getComputerChoice())
+//Plays 5 rounds of rock, paper, scissors. Calls the playRound function and records the winner of that round in a variable. print the winner of five rounds in the console
+function game() {
+    let playerWins = 0
+    let computerWins = 0
 
-console.log(playerChoice)
-console.log(computerChoice)
+    const playerChoice = getHandChoice(1)
 
-playRound(playerChoice, computerChoice)
+    for (let i = 1; i <= 5; i++) {
+        let computerChoice = getHandChoice(getComputerChoice())
+        console.log(`Round ${i}:`)
+
+        let playResult = playRound(playerChoice, computerChoice)
+
+        if (playResult === 1) {
+            playerWins++
+        }
+        else if (playResult === 0) {
+            computerWins++
+        }
+        else {
+            continue
+        }
+    }
+
+    if (playerWins > computerWins) {
+        console.log("Player wins")
+    }
+    else {
+        console.log("Computer wins")
+    }
+    
+}
+
+game()
