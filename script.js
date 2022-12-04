8a991a3c5fc520796c3b5c7bad11158a06d15b6f
Michael Chan <michaelalexanderchan@gmail.com>
2022-12-03 20:42:35 -0500

    Reworked game logic, added eventHandles to buttons, and added html elements using js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//Global variable to track the round number
+let currentRound = 0;
+
 //Returns a value between 1 - 3 that represents the computer hand choice
 function getComputerChoice() {
     return Math.floor(Math.random() * 3) + 1
@@ -8,16 +11,16 @@ function getHandChoice(number) {
     let hand
     let convertNumber = Number(number)
     if (convertNumber === 1) {
-        hand = "Rock"
+        hand = "rock"
     }
     else if (convertNumber === 2) {
-        hand = "Paper"
+        hand = "paper"
     }
     else if (convertNumber === 3) {
-        hand = "Scissors"
+        hand = "scissors"
     }
     else {
-        hand = "Invalid"
+        hand = "invalid"
     }
     return hand
 }
@@ -25,8 +28,8 @@ function getHandChoice(number) {
 //Compares the player's choice to the computer's. If the player won, return true. Otherwise, return false
 function playerWin(playerChoice, computerChoice) {
     let playerWin
-    if (playerChoice === "Rock") {
-        if (computerChoice === "Paper"){
+    if (playerChoice === "rock") {
+        if (computerChoice === "paper") {
             playerWin = false
         }
         else {
@@ -34,8 +37,8 @@ function playerWin(playerChoice, computerChoice) {
         }
     }
 
-    else if (playerChoice === "Paper") {
-        if (computerChoice === "Scissors") {
+    else if (playerChoice === "paper") {
+        if (computerChoice === "scissors") {
             playerWin = false
         }
         else {
@@ -43,8 +46,8 @@ function playerWin(playerChoice, computerChoice) {
         }
     }
 
-    else if (playerChoice === "Scissors") {
-        if (computerChoice === "Rock") {
+    else if (playerChoice === "scissors") {
+        if (computerChoice === "rock") {
             playerWin = false
         }
         else {
@@ -57,45 +60,44 @@ function playerWin(playerChoice, computerChoice) {
 
 //Plays a round of rock, paper scissors. Calls the PlayerWin function and logs the winner in the console. Returns 1 for player win, 0 for computer win
 function playRound(playerChoice, computerChoice) {
+    let roundResult;
+    let winner = 0;
     if (playerChoice === computerChoice) {
-        console.log(`It's a tie!`)
-        return -1
+        roundResult = `It's a tie!`
     }
     else if (playerWin(playerChoice, computerChoice)) {
-        console.log(`You win! ${playerChoice} beats ${computerChoice}`)
-        return 1
+        roundResult = `You win! ${(playerChoice.charAt(0).toUpperCase()) + playerChoice.slice(1)} beats ${(computerChoice.charAt(0).toUpperCase()) + computerChoice.slice(1)}`;
+        winner = 1;
     }
     else {
-        console.log(`You lose! ${computerChoice} beats ${playerChoice}`) 
-        return 0
+        roundResult = `You lose! ${(computerChoice.charAt(0).toUpperCase()) + computerChoice.slice(1)} beats ${(playerChoice.charAt(0).toUpperCase()) + playerChoice.slice(1)}`;
+        winner = -1
     }
+
+    resultText.textContent = roundResult;
+    return winner;
 }
 
 //Plays 5 rounds of rock, paper, scissors. Calls the playRound function and records the winner of that round in a variable. print the winner of five rounds in the console
-function game() {
+function game(item) {
+    currentRound += 1;
     let playerWins = 0
     let computerWins = 0
 
-    //Loop for 5 rounds
-    // for (let i = 1; i <= 5; i++) {
-    //     let playerChoice = prompt("Please enter a number for your choice:\n1 - Rock\n2 - Paper\n3 - Scissors?")
-    //     playerChoice = getHandChoice(playerChoice)
+    let playerChoice = item;
 
-    //     let computerChoice = getHandChoice(getComputerChoice())
-    //     console.log(`Round ${i}:`)
+    let computerChoice = getHandChoice(getComputerChoice())
+    roundNumber.textContent = `Round ${currentRound}:`;
 
-    //     let playResult = playRound(playerChoice, computerChoice)
+    let playResult = playRound(playerChoice, computerChoice)
 
-    //     if (playResult === 1) {
-    //         playerWins++
-    //     }
-    //     else if (playResult === 0) {
-    //         computerWins++
-    //     }
-    //     else {
-    //         continue
-    //     }
-    // }
+    if (playResult === 1) {
+        playerWins++
+    }
+    else if (playResult === -1) {
+        computerWins++
+    }
+
 
     if (playerWins === computerWins) {
         console.log("Tie!")
@@ -106,7 +108,26 @@ function game() {
     else {
         console.log("Computer wins")
     }
-    
 }
 
-game()
+let roundNumber = document.createElement('h1')
+roundNumber.setAttribute('id', 'roundNumber');
+document.body.appendChild(roundNumber);
+
+let resultNode = document.createElement('div');
+resultNode.setAttribute('id', 'resultdiv');
+let resultText = document.createElement('p');
+resultNode.appendChild(resultText);
+document.body.appendChild(resultNode);
+
+let buttons = document.querySelectorAll('.btn');
+
+buttons.forEach((item) => {
+    item.addEventListener('click', () => {
+        game(item.id)
+     })
+})
+
+
+
+
